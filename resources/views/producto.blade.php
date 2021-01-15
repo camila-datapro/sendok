bf33218f956428d4f7e87c29d3760b3834c9b938
camila-datapro <cfigueroa@datapro.cl>
2021-01-15 14:01:14 -0300

    Cambios de estilo menu

--- resources/views/producto.blade.php
+++ resources/views/producto.blade.php
@@ -5,22 +5,14 @@
       <meta charset="utf-8">
       <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
       <title>Sendok</title>
-      <!-- plugins:css -->
-      <link rel="stylesheet" href=" {{ asset('/assets/vendors/iconfonts/mdi/css/materialdesignicons.min.css') }}">
+      <link rel="stylesheet" href="{{ asset('/assets/vendors/iconfonts/mdi/css/materialdesignicons.min.css') }}">
       <link rel="stylesheet" href="{{ asset('/assets/vendors/iconfonts/ionicons/css/ionicons.css') }}">
       <link rel="stylesheet" href="{{ asset('/assets/vendors/iconfonts/typicons/src/font/typicons.css') }}">
       <link rel="stylesheet" href="{{ asset('/assets/vendors/iconfonts/flag-icon-css/css/flag-icon.min.css') }}">
       <link rel="stylesheet" href="{{ asset('/assets/vendors/css/vendor.bundle.base.css') }}">
       <link rel="stylesheet" href="{{ asset('/assets/vendors/css/vendor.bundle.addons.css') }}">
-      <!-- endinject -->
-      <!-- plugin css for this page -->
-      <!-- End plugin css for this page -->
-      <!-- inject:css -->
       <link rel="stylesheet" href="{{ asset('/assets/css/shared/style.css') }}">
-      <!-- endinject -->
-      <!-- Layout styles -->
       <link rel="stylesheet" href="{{ asset('/assets/css/demo_1/style.css') }}">
-      <!-- End Layout styles -->
       <link rel="shortcut icon" href="{{ asset('/assets/images/favicon.png') }}" />
    </head>
    <body>
@@ -244,20 +236,11 @@
             </div>
          </div>
          </div>
-      <!-- container-scroller -->
-      <!-- plugins:js -->
       <script src="{{ asset('/assets/vendors/js/vendor.bundle.base.js') }}"></script>
       <script src="{{ asset('/assets/vendors/js/vendor.bundle.addons.js') }}"></script>
-      <!-- endinject -->
-      <!-- Plugin js for this page-->
-      <!-- End plugin js for this page-->
-      <!-- inject:js -->
       <script src="{{ asset('/assets/js/shared/off-canvas.js') }}"></script>
       <script src="{{ asset('/assets/js/shared/misc.js') }}"></script>
-      <!-- endinject -->
       <script src="{{ asset('/js/producto.js') }}"></script>
-      <!-- Custom js for this page-->
       <script src="{{ asset('/assets/js/demo_1/dashboard.js') }}"></script>
-      <!-- End custom js for this page-->
    </body>
 </html>
