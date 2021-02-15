c10cb7c960ff641ee2b084416b640c4006e13197
camila-datapro <cfigueroa@datapro.cl>
2021-02-15 13:12:04 -0300

    se remueve log de debug en producto

--- public/js/producto.js
+++ public/js/producto.js
@@ -176,8 +176,6 @@ function cargarTiposDeProducto(){
   function confirmarEliminacion(producto){
     id_boton = producto.id_producto;
     nombre_producto = producto.nombre_producto;
-  
-    console.log("id boton: "+id_boton+"nombre producto:"+ nombre_producto);
     var id_producto = parseInt(id_boton);
     $("#modal_eliminar_nombre").text(nombre_producto);
     $("#modal_eliminar").removeAttr("id_producto");
