afec3ef7cd3897c241bc9931d5aae7adb034ccc6
camila-datapro <cfigueroa@datapro.cl>
2021-02-22 18:07:09 -0300

    Traduccion de mensaje de error de password

--- resources/lang/en/auth.php
+++ resources/lang/en/auth.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'failed' => 'These credentials do not match our records.',
-    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+    'failed' => 'Contraseña no válida, intente nuevamente.',
+    'throttle' => 'Se han producido muchos intentos de inicio de sesión, porfavor espere :seconds segundos.',
 
 ];
